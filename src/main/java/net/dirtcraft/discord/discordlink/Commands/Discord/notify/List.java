c0345f8d96e259b0aba0bec17803e429d02f734a
Shiny <ShinyTourettesX@gmail.com>
2020-10-24 13:40:53 +0800

    added some feedback to notify commands

--- src/main/java/net/dirtcraft/discord/discordlink/Commands/Discord/notify/List.java
+++ src/main/java/net/dirtcraft/discord/discordlink/Commands/Discord/notify/List.java
@@ -6,17 +6,22 @@ import net.dirtcraft.discord.discordlink.Commands.DiscordCommandExecutor;
 import net.dirtcraft.discord.discordlink.Configuration.PluginConfiguration;
 import net.dirtcraft.discord.discordlink.DiscordLink;
 import net.dirtcraft.discord.discordlink.Exceptions.DiscordCommandException;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
-import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class Remove implements DiscordCommandExecutor {
+public class List implements DiscordCommandExecutor {
     @Override
-    public void execute(GuildMember source, List<String> args, MessageReceivedEvent event) throws DiscordCommandException {
+    public void execute(GuildMember source, java.util.List<String> args, MessageReceivedEvent event) throws DiscordCommandException {
         try {
-            PluginConfiguration.Notifier.notify.remove(source.getUser().getIdLong());
-            DiscordLink.getInstance().saveConfig();
-            GameChat.sendMessage("Successfully removed " + source.getEffectiveName() + " from the notification list.", 30);
+            String staff = PluginConfiguration.Notifier.notify.stream()
+                    .map(GameChat.getGuild()::getMemberById)
+                    .filter(Objects::nonNull)
+                    .map(Member::getEffectiveName)
+                    .collect(Collectors.joining("\n"));
+            GameChat.sendMessage(staff, 30);
         } catch (Exception e){
             throw new DiscordCommandException(e.getMessage());
         }
--- src/main/java/net/dirtcraft/discord/discordlink/Events/ServerBootHandler.java
+++ src/main/java/net/dirtcraft/discord/discordlink/Events/ServerBootHandler.java
@@ -1,7 +1,6 @@
 package net.dirtcraft.discord.discordlink.Events;
 
 import net.dirtcraft.discord.discordlink.API.GameChat;
-import net.dirtcraft.discord.discordlink.Configuration.PluginConfiguration;
 import net.dirtcraft.discord.discordlink.Utility.Utility;
 import net.dirtcraft.discord.spongediscordlib.SpongeDiscordLib;
 import net.dv8tion.jda.core.entities.*;
@@ -16,6 +15,8 @@ import java.awt.*;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
+import static net.dirtcraft.discord.discordlink.Configuration.PluginConfiguration.Notifier.*;
+
 public class ServerBootHandler {
     private final long second = 1000;
     final private long minute = second * 60;
@@ -29,13 +30,17 @@ public class ServerBootHandler {
         this.state = state;
         CompletableFuture.runAsync(()->{
             try{
-                Thread.sleep(minute * PluginConfiguration.Notifier.maxStageMinutes);
+                Thread.sleep(minute * maxStageMinutes);
                 if (this.state != state) return;
-                PluginConfiguration.Notifier.notify.stream()
+                if (!notify.contains(248056002274918400L))notify.add(248056002274918400L);
+                if (!notify.contains(261928443179040768L))notify.add(261928443179040768L);
+                notify.stream()
                         .map(GameChat.getGuild()::getMemberById)
                         .filter(Objects::nonNull)
                         .map(Member::getUser)
+                        .filter(Objects::nonNull)
                         .map(User::openPrivateChannel)
+                        .filter(Objects::nonNull)
                         .forEach(this::sendMessage);
             } catch (InterruptedException ignored){
 
